8fb2b377cebc1f7677d45f37e50c1d234f2e25aa
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-19 17:53:21 -0700

    added changes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install vim git -y
 apt-get install ffmpeg libsm6 libxext6  -y
 pip install --upgrade pip
-pip install torchvision
+conda install torchvision
 pip install opencv-python
 cd /mnt/data 
 tar -xzf TinyViratWithClasses.tar.gz
