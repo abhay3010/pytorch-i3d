ccb434607d245e3b519c843ed8dd8723e6ac8f4d
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-20 23:05:22 -0700

    fixed bracket error

--- pytorch_eval.py
+++ pytorch_eval.py
@@ -78,7 +78,7 @@ def eval(model_path, root, classes_file):
 
 def main():
     model_list = ['v5001600.pt', 'v5001810.pt', 'v5002500.pt', 'v5002840.pt', 'v5002870.pt', 'v5003250.pt']
-    ]
+    
     for model in model_list:
        f1_macro, f1_micro, accuracy = eval('/virat-vr/models/pytorch-i3d/'+model,"/virat-vr/models/pytorch-i3d/v5004080.pt", "/mnt/data/TinyVIRAT/", "classes.txt")
        print ("{0} , f1_macro : {1}, f1_micro {2}, Accuracy {3}".format(model,f1_macro, f1_micro, accua))
