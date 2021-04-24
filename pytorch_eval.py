c1ecf69ebba13984b83c041a71db8c5b093b6c01
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-23 21:21:05 -0700

    adding randomization in the evaluation of the test set

--- pytorch_eval.py
+++ pytorch_eval.py
@@ -80,4 +80,4 @@ def main():
        f1_macro, f1_micro, accuracy = eval('/virat-vr/models/pytorch-i3d/'+model, "/mnt/data/TinyVIRAT/", "classes.txt")
        print ("{0} , f1_macro : {1}, f1_micro {2}, Accuracy {3}".format(model,f1_macro, f1_micro, accuracy))
 if __name__ == '__main__':
-    main()
+    main()  
