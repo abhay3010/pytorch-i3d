32fa5e3c78b87ce7f7c332a7f203840f907289d1
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-24 11:59:17 -0700

    changed to ordered dict import

--- model_utils.py
+++ model_utils.py
@@ -1,11 +1,12 @@
 """
     Utilities for loading and freezing the parameters of a model
 """
+from collections import OrderedDict
 import torch
 #Hacky: Will need to be fixed later
 def load_params_from_file(model, params_file, device):
     state_dict = torch.load(params_file, map_location=device)
-    new_dict = dict()
+    new_dict = OrderedDict()
     for k, v in state_dict.items():
         if k.startswith("module."):
             new_dict[k[7:]] = v
