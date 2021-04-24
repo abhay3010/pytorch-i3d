2c3970c648de57d0fcd962d35b8756262fe9a373
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-24 14:23:14 -0700

    revered loading code

--- model_utils.py
+++ model_utils.py
@@ -6,6 +6,7 @@ import torch
 #Hacky: Will need to be fixed later
 def load_params_from_file(model, params_file, device):
     print("loading", params_file)
+    print("loading device", device)
     state_dict = torch.load(params_file, map_location=device)
     
     new_dict = OrderedDict()
