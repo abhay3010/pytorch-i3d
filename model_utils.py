f85e135b7cf3832996c70d84d3dfdffb67ad9f81
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-24 13:00:53 -0700

    hacking model load

--- model_utils.py
+++ model_utils.py
@@ -22,15 +22,15 @@ def load_module_params_from_file(model, params_file, device):
     print("loading", params_file)
     state_dict = torch.load(params_file)
     
-    new_dict = OrderedDict()
-    for k, v in state_dict.items():
-        if k.startswith("module."):
-            print("updating", k, "to ", k[7:])
-            new_dict[k[7:]] = v
-        else:
-            print(k)
-            new_dict[k] = v
-    model.load_state_dict(new_dict)
+    # new_dict = OrderedDict()
+    # for k, v in state_dict.items():
+    #     if k.startswith("module."):
+    #         print("updating", k, "to ", k[7:])
+    #         new_dict[k[7:]] = v
+    #     else:
+    #         print(k)
+    #         new_dict[k] = v
+    model.module.load_state_dict(state_dict)
     print("done loading model")
     return model
         
