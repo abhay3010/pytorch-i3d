3281dd82fb517309d5ae8b1d799f54312bdc4212
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-25 09:59:32 -0700

    added eval resizer job and modified setup.sh to use new docker image

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 apt-get update
-apt-get install vim git -y
-apt-get install ffmpeg libsm6 libxext6  -y
-pip install --upgrade pip
-# conda install torchvision
-pip install torchvision
-pip install opencv-python
-pip install sklearn
-pip install torchsummary
+# apt-get install vim git -y
+# apt-get install ffmpeg libsm6 libxext6  -y
+# pip install --upgrade pip
+# # conda install torchvision
+# pip install torchvision
+# pip install opencv-python
+# pip install sklearn
+# pip install torchsummary
 cd /mnt/data 
 tar -xzf TinyViratWithClasses.tar.gz
 cd $2
