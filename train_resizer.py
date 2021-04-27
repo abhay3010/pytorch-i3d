c12ff4130aa07a67e29f44d5ed00e55118681902
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-26 23:24:04 -0700

    changed resizer channels

--- train_resizer.py
+++ train_resizer.py
@@ -29,7 +29,7 @@ from virat_dataset import collate_tensors
 import numpy as np
 
 from i3d import InceptionI3d
-from resizer import ResizerMainNetwork
+from resizer import ResizerMainNetworkV2
 
 from virat_dataset import Virat as Dataset
 
@@ -42,7 +42,7 @@ def run(data_root, model_input_shape, virat_model_path,batch_size,save_model='',
     print("declared model")
     i3d = load_params_from_file(i3d, virat_model_path, device)
     #load the resizer_model
-    resizer = ResizerMainNetwork(3, int(v_mode.split('x')[0]), model_input_shape)
+    resizer = ResizerMainNetworkV2(3, int(v_mode.split('x')[0]), model_input_shape)
     #load the virat dataset
     train_transforms = transforms.Compose([ videotransforms.RandomHorizontalFlip(),
     ])
@@ -126,7 +126,7 @@ def main():
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt'
     batch_size = 4
-    save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_v5'
+    save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_v2_v6'
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
 
