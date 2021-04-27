0776137348e08ed509d614652cd36ce92a977897
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-26 23:20:19 -0700

    modified resizer to use 16 channels

--- resizer.py
+++ resizer.py
@@ -143,6 +143,50 @@ class ResizerMainNetwork(nn.Module):
             out+=residual
             return out
 
+class ResizerMainNetworkV2(nn.Module):
+    def __init__(self, in_channels,n_frames, scale_shape,num_resblocks=1, skip=False):
+        self.in_channels = in_channels
+        self.r = num_resblocks
+        self.scale_shape = scale_shape
+        self.nframes = n_frames
+        self.skip = skip
+        super(ResizerMainNetworkV2, self).__init__()
+        self.skip_resizer =  ResizerBlock((self.nframes,)+self.scale_shape, False)
+        if not self.skip:
+            self.c1 = ConvUnit(in_channels=self.in_channels, output_channels=16, kernel_shape=[7, 7, 7],  norm=None)
+            #revisit size of this unit as it is inconsitent between paper and diagram
+            self.c2 = ConvUnit(in_channels=16, kernel_shape = [1,1,1], output_channels=16)
+            self.resizer_first = ResizerBlock((self.nframes,) + self.scale_shape, False)
+            self.residual_blocks = make_residuals(num_resblocks, 16)
+            self.c3 = ConvUnit(in_channels=16, kernel_shape=[3,3,3], output_channels=16, lru=False)
+            self.c4 = ConvUnit(in_channels=16, kernel_shape=[3,3,3], output_channels=self.in_channels, lru=False, norm=None)
+        
+    def forward(self, x):
+        # print("input shape", x.shape)
+        residual = self.skip_resizer(x)
+        if self.skip:
+            return residual
+        else:
+
+        # print("resizer_shape", out.shape)
+            out = self.c1(x)
+            # print("conv shape", out.shape)
+
+            out = self.c2(out)
+            # print("conv2 shape", out.shape)
+            out =  self.resizer_first(out)
+            # print("in resizer shape", out.shape)
+            residual_skip = out
+            out = self.residual_blocks(out)
+            out = self.c3(out)
+            out+=residual_skip
+            # print(out.shape)        
+            out = self.c4(out)
+            # print(out.shape)
+            out+=residual
+            return out
+
+
 class ResidualBlock(nn.Module):
     def __init__(self,num_channels):
         super(ResidualBlock, self).__init__()
@@ -168,8 +212,8 @@ def make_residuals(r, in_channels):
     return nn.Sequential(*residuals)
 
 def main():
-    resizer_network = ResizerMainNetwork(3,32, (112,112) )
-    summary(resizer_network, (3, 32, 28, 28))
+    resizer_network = ResizerMainNetworkV2(3,32, (112,112) )
+    summary(resizer_network, (3, 32, 28, 28), batch_size=2)
 if __name__ == '__main__':
     main()
     
