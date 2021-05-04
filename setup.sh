bedcd07c1106d439b9ffa852c4926882ad3f4f08
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-03 23:50:48 -0700

    added extra copy in setup

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ cp /mnt/data/TinyVIRAT/videos/train/VIRAT_S_000001/3759_frames/frame_75.jpg /mnt
 cp /mnt/data/TinyVIRAT/videos/train/VIRAT_S_040104_05_000939_001116/6252_frames/frame_94.jpg /mnt/data/TinyVIRAT/videos/train/VIRAT_S_040104_05_000939_001116/6252_frames/frame_95.jpg
 cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_112.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_113.jpg
 cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_112.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_114.jpg
+cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_114.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040103_00_000000_000120/2165_frames/frame_115.jpg
 cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_104.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_105.jpg
 cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_105.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_106.jpg
 cp /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_106.jpg /mnt/data/TinyVIRAT/videos/test/VIRAT_S_040000_00_000000_000036/3410_frames/frame_107.jpg
