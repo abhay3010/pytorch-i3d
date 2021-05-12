b50e846a246d6a7a10b68bcde811ee708b2bdf66
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-12 14:00:39 -0700

    added a more aggressive learning rate

--- train_resnet_batch.py
+++ train_resnet_batch.py
@@ -52,7 +52,7 @@ def train_model(model, dataloaders, criterion, optimizer, model_prefix='', num_e
             else:
                 torch.save(model.state_dict(), model_prefix  + str(epoch).zfill(6)+'.pt')
 
-def run(root, classes_file,save_path, batch_size=256, lr=0.001):
+def run(root, classes_file,save_path, batch_size=256, lr=0.005):
     #Initialise the dataset, loaders and model with the right set of parameters. 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     train_transforms = transforms.Compose([ 
