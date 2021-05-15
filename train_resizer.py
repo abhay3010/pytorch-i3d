c68a2eb3b9bf9ba7c409d5bbac4307bab9ca11ca
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-15 13:59:24 -0700

    fixed local

--- train_resizer.py
+++ train_resizer.py
@@ -129,11 +129,11 @@ def main():
     save_model = 'bilinear_32_resizer_v1'
 
     #GPU parameters
-    # data_root = '/mnt/data/TinyVIRAT/'
-    # model_input_shape = (112, 112)
-    # virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_32_112002400.pt'
-    # batch_size = 4
-    # save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_v3_v8_final'
+    data_root = '/mnt/data/TinyVIRAT/'
+    model_input_shape = (112, 112)
+    virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_32_112002400.pt'
+    batch_size = 4
+    save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_v3_v8_final'
 
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
