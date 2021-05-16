2c89f8e29050e20594b414f3476f611bd7ddf05d
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-16 13:24:57 -0700

    resizer changes for next run

--- train_resizer_time.py
+++ train_resizer_time.py
@@ -133,7 +133,7 @@ def main():
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_16_112002800.pt'
     batch_size = 32
-    save_model = '/virat-vr/models/pytorch-i3d/bilinear_16_resizer_timecompression_v1'
+    save_model = '/virat-vr/models/pytorch-i3d/bilinear_16_resizer_timecompression_v2'
 
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
