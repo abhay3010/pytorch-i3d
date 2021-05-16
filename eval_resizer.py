4717fba69c8301dd40e036d233b602b88a2d75c7
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-15 19:20:17 -0700

    reduced batch size

--- eval_resizer.py
+++ eval_resizer.py
@@ -42,7 +42,7 @@ def eval(resizer_model, model_path, root, classes_file):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     val_dataset = Dataset(root, "test",classes_file, resize=False, transforms=None)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=4, pin_memory=True, collate_fn=collate_tensors) 
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=4, pin_memory=True, collate_fn=collate_tensors) 
     resizer = ResizerMainNetworkV2(3, 32, (112, 112), skip=False)
     resizer.load_state_dict(torch.load(resizer_model))
     resizer.to(device)
