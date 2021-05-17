b31ed94bdd7f4a91171c1e4ca0afb89016f10136
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-17 12:09:26 -0700

    reset weight decay

--- train_resizer_sgd.py
+++ train_resizer_sgd.py
@@ -68,7 +68,7 @@ def run(data_root, model_input_shape, virat_model_path,batch_size,save_model='',
         if "logits" not in name:
             param.requires_grad= False
     
-    optimizer = optim.SGD(list(resizer.parameters()) + list(i3d.parameters()), lr=lr, momentum=0.9, weight_decay=0.000001)
+    optimizer = optim.SGD(list(resizer.parameters()) + list(i3d.parameters()), lr=lr, momentum=0.9, weight_decay=0.0000001)
     lr_sched = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 12, T_mult=2, eta_min=1e-9, last_epoch=-1, verbose=False)
 
     print("resizer network", resizer)
