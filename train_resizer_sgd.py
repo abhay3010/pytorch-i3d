9bf51274c6a7b2896e1b8fcf27b51175355f98f3
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-17 14:31:30 -0700

    added test to

--- train_resizer_sgd.py
+++ train_resizer_sgd.py
@@ -137,7 +137,7 @@ def main():
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt'
     batch_size = 4
-    save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_sgd_v4_v9_final_resizer_v2_rerun'
+    save_model = '/virat-vr/models/pytorch-i3d/bilinear_32_resizer_sgd_v4_v9_final_resizer_v2_rerun_test'
 
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
