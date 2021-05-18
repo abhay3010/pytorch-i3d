559a9c1b3ffa44c8e240ef1954a46b920d009143
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-18 14:12:33 -0700

    Fixed keyerror

--- virat_dataset.py
+++ virat_dataset.py
@@ -125,7 +125,7 @@ class Virat(data_util.Dataset):
         start_f = 0
         num_frames = self.num_frames
         if self.load_all:
-            num_frames = min(details['frames', self.num_frames])
+            num_frames = min(details['frames'], self.num_frames)
         if self.shuffule:
             try:
                 start_f = random.randint(0,details['frames']-self.num_frames-1)
