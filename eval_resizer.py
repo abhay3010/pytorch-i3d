28a264f44095dca9ebfb0304b37735539b19173d
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-19 14:52:47 -0700

    added changes for resnet validation

--- eval_resizer.py
+++ eval_resizer.py
@@ -43,7 +43,7 @@ def eval(resizer_model, model_path, root, classes_file):
 
     val_dataset = Dataset(root, "test",classes_file, resize=False, transforms=None)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=4, pin_memory=True, collate_fn=collate_tensors) 
-    resizer = ResizerMainNetworkV4(3, 32, (112, 112), skip=False, num_resblocks=3)
+    resizer = ResizerMainNetworkV4_3D(3, 32, (112, 112), skip=False, num_resblocks=2)
     resizer.load_state_dict(torch.load(resizer_model))
     resizer.to(device)
    
@@ -89,7 +89,7 @@ def eval(resizer_model, model_path, root, classes_file):
 
 def main():
     #i3d_model = "/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt"
-    prefix = 'bilinear_32_resizer_v9_final_resizer_v43r_rerun'
+    prefix = 'bilinear_32_resizer_v9_final_resizer_v43r_residuals'
     model_list = list()
     for epoch in range(2,15):
         model_list.append((prefix+str(epoch).zfill(6)+'.pt', prefix+ 'i3d'+str(epoch).zfill(6)+'.pt'))
