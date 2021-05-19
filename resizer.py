b2c3ae6053d179739937c9ba0bcbe55272389b91
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-19 09:23:49 -0700

    resizer network with channels as 1 x x

--- resizer.py
+++ resizer.py
@@ -256,13 +256,13 @@ class ResizerMainNetworkV4(nn.Module):
         super(ResizerMainNetworkV4, self).__init__()
         self.skip_resizer =  ResizerBlock((self.nframes,)+self.scale_shape, False)
         if not self.skip:
-            self.c1 = ConvUnit(in_channels=self.in_channels, output_channels=16, kernel_shape=[7, 7, 7],  norm=None)
+            self.c1 = ConvUnit(in_channels=self.in_channels, output_channels=16, kernel_shape=[1, 7, 7],  norm=None)
             #revisit size of this unit as it is inconsitent between paper and diagram
             self.c2 = ConvUnit(in_channels=16, kernel_shape = [1,1,1], output_channels=16)
             self.resizer_first = ResizerBlock((self.nframes,) + self.scale_shape, False)
-            self.residual_blocks = make_residuals(num_resblocks, 16)
-            self.c3 = ConvUnit(in_channels=16, kernel_shape=[3,3,3], output_channels=16, lru=False)
-            self.c4 = ConvUnit(in_channels=16, kernel_shape=[7,7,7], output_channels=self.in_channels, lru=False, norm=None)
+            self.residual_blocks = make_residuals_2d(num_resblocks, 16)
+            self.c3 = ConvUnit(in_channels=16, kernel_shape=[1,3,3], output_channels=16, lru=False)
+            self.c4 = ConvUnit(in_channels=16, kernel_shape=[1,7,7], output_channels=self.in_channels, lru=False, norm=None)
         
     def forward(self, x):
         # print("input shape", x.shape)
