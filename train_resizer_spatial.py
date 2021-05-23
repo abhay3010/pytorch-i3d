fb509b4c84604f32ba8f70129b345e53a569af22
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-23 12:28:44 -0700

    job to start spatial training added

--- train_resizer_spatial.py
+++ train_resizer_spatial.py
@@ -139,7 +139,7 @@ def main():
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt'
     batch_size =8
-    save_model = '/virat-vr/models/pytorch-i3d/32_112_2res_spatial_test'
+    save_model = '/virat-vr/models/pytorch-i3d/32_112_2res_spatial'
 
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
