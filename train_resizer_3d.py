9f05d43a1590b78b01c4ad7c53616c3e8d471860
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-26 16:03:34 -0700

    reduced batch size to 32

--- train_resizer_3d.py
+++ train_resizer_3d.py
@@ -133,7 +133,7 @@ def main():
     data_root = '/mnt/data/TinyVIRAT/'
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/i3d_inp112_002300.pt'
-    batch_size = 40
+    batch_size = 32
     save_model = '/virat-vr/models/pytorch-i3d/resizerv43d_112_32_'
 
     num_epochs=50
