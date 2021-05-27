cfe36b50d471cc9a0a0f24fa4b9f5fa3acf0be1b
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-05-27 06:36:13 -0700

    increased batch size

--- train_resizer_spatial.py
+++ train_resizer_spatial.py
@@ -139,7 +139,7 @@ def main():
     data_root = '/mnt/data/TinyVIRAT/'
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/i3d_inp28_002400.pt'
-    batch_size =16
+    batch_size =40
     save_model = '/virat-vr/models/pytorch-i3d/inp28_2dsp_2d_resizer'
 
     num_epochs=50
