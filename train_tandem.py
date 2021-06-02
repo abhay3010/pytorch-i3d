3ae5b131f74de405a3666ded02fb597b999fbe25
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-02 15:38:10 -0700

    updated tandem weigts and added annealing to learning rate

--- train_tandem.py
+++ train_tandem.py
@@ -18,7 +18,7 @@ from spatial_transformer import *
 
 
 
-def train_model(model, dataloaders, criterion, optimizer,model_prefix='', num_epochs=50  ):
+def train_model(model, dataloaders, criterion, optimizer, scheduler = None, model_prefix='', num_epochs=50, weights=torch.ones(26)  ):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs))
@@ -37,7 +37,7 @@ def train_model(model, dataloaders, criterion, optimizer,model_prefix='', num_ep
                 with torch.set_grad_enabled(phase=='train'):
 
                     outputs = model(inputs)
-                    loss = criterion(outputs, labels)
+                    loss = criterion(outputs, labels,weights)
                     if phase == 'train':    
                         loss.backward()     
                         optimizer.step()
@@ -45,8 +45,11 @@ def train_model(model, dataloaders, criterion, optimizer,model_prefix='', num_ep
                 counter+=1
                 if counter%10 == 0:
                     print("step ", counter, running_loss/(counter*inputs.size(0)))
+                if scheduler and phase == 'train':
+                    scheduler.step()
             epoch_loss = running_loss /len(dataloaders[phase].dataset)
             print('{} Loss: {:.4f} '.format(phase, epoch_loss) )
+
         if phase == 'val':
             if isinstance(model, nn.DataParallel):
                 torch.save(model.module.state_dict(), model_prefix+str(epoch).zfill(6)+'.pt')
@@ -69,12 +72,15 @@ def run(root, classes_file, save_path, resizer_path, i3d_path,v_mode='32x112',mo
     actual_model = load_actual_model(v_mode, model_input_shape)
     model_ft = TwoStreamNetwork(actual_model, helper_model, 26)
     optimizer_ft = optim.Adam(model_ft.get_parameters_to_train(), lr=lr)
+    scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer_ft, 2, 2, eta_min=0.0000001)
     ## define the criteria
     criterion  = nn.BCEWithLogitsLoss()
     model_ft.to(device)
     if torch.cuda.device_count()>1:
         model_ft = nn.DataParallel(model_ft)
-    train_model(model_ft, dataloaders,criterion, optimizer_ft, model_prefix=save_path )
+    weights = torch.FloatTensor([1,1,0,0,1,1,1,0,1,0,1,1,0,1,1,0,0,1,1,1,0,0,0,0,1,1])
+    weights.to(device)
+    train_model(model_ft, dataloaders,criterion, optimizer_ft,scheduler, model_prefix=save_path, weights=weights )
 
 def load_resizer_and_i3d(resizer_path, i3d_path, device):
     i3d = InceptionI3d(26, mode="32x112", in_channels=3)
