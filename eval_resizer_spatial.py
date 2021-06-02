8428af9b132f0a04ea93b9f629ad9883ccdd2be3
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-02 11:18:19 -0700

    resizer spatial

--- eval_resizer_spatial.py
+++ eval_resizer_spatial.py
@@ -108,7 +108,7 @@ def main():
     #i3d_model = "/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt"
     prefix = 'imp112_wtithout_resizer_'
     model_list = list()
-    for epoch in range(5,8):
+    for epoch in range(8, 21):
         model_list.append((prefix+str(epoch).zfill(6)+'.pt', prefix+ 'i3d'+str(epoch).zfill(6)+'.pt'))
     for model, i3d_model in model_list:
        f1_macro, f1_micro, accuracy = eval('/virat-vr/models/pytorch-i3d/'+ model, '/virat-vr/models/pytorch-i3d/'+ i3d_model, "/mnt/data/TinyVIRAT/", "classes.txt", debug=True)
