0bcbe715a01e34f9c99cfb0f12afa8e72be997eb
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-02 09:33:16 -0700

    added changes for increased batch size

--- train_resizer_spatial.py
+++ train_resizer_spatial.py
@@ -140,7 +140,7 @@ def main():
     data_root = '/mnt/data/TinyVIRAT/'
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/i3d_inp112_002300.pt'
-    batch_size =32
+    batch_size =40
     save_model = '/virat-vr/models/pytorch-i3d/imp112_wtithout_resizer_'
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
