171f9187271e7141e45e3e5da2e08c0dc06badb0
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-03 05:59:06 -0700

    added eval script

--- eval_resizer_spatial.py
+++ eval_resizer_spatial.py
@@ -42,10 +42,10 @@ from virat_dataset import collate_tensors, load_rgb_frames
 def eval(resizer_model, model_path, root, classes_file,v_mode="32x112", debug=False):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    val_dataset = Dataset(root, "test",classes_file,num_frames=32, resize_shape=(28,28), transforms=None)
+    val_dataset = Dataset(root, "test",classes_file,num_frames=32, resize_shape=(112,112), transforms=None)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=12, shuffle=False, num_workers=4, pin_memory=True) 
     resizer = nn.Sequential(
-        SpatialTransformer(3, in_time=int(v_mode.split('x')[0]), in_res=28),
+        SpatialTransformer(3, in_time=int(v_mode.split('x')[0]), in_res=112),
         ResizerMainNetworkV4_3D(3, int(v_mode.split('x')[0]), (112,112),num_resblocks=1)
         
     )
@@ -106,7 +106,7 @@ def eval(resizer_model, model_path, root, classes_file,v_mode="32x112", debug=Fa
 
 def main():
     #i3d_model = "/virat-vr/models/pytorch-i3d/v7_bilinear_32_112004400.pt"
-    prefix = 'spatial_resizer3d_28_'
+    prefix = 'spatial_2d_2dr_112_'
     model_list = list()
     for epoch in range(4, 50):
         model_list.append((prefix+str(epoch).zfill(6)+'.pt', prefix+ 'i3d'+str(epoch).zfill(6)+'.pt'))
