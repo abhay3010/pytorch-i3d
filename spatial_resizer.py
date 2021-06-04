63cff45db8e97f9561c37f0de855093c5b8fcffb
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-04 12:09:35 -0700

    align corners is true

--- spatial_resizer.py
+++ spatial_resizer.py
@@ -95,8 +95,8 @@ class TransformerWithResizer(nn.Module):
         h = x.shape[3]
         w = x.shape[4]
         x_view = x.view(-1,c,h,w)
-        grid = F.affine_grid(theta, x_view.size(),align_corners=False)
-        x_view = F.grid_sample(x_view, grid, align_corners=False)
+        grid = F.affine_grid(theta, x_view.size(),align_corners=True)
+        x_view = F.grid_sample(x_view, grid, align_corners=True)
         o = x_view.view(b,c,t,h,w)
         return o
     
