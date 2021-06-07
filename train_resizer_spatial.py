34e5bbaa992b61081efebf56be8956133dcad4d4
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-07 08:49:16 -0700

    added changes

--- train_resizer_spatial.py
+++ train_resizer_spatial.py
@@ -136,7 +136,7 @@ def main():
     data_root = '/mnt/data/TinyVIRAT/'
     model_input_shape = (112, 112)
     virat_model_path = '/virat-vr/models/pytorch-i3d/i3d_inp28_002400.pt'
-    batch_size =42
+    batch_size =32
     save_model = '/virat-vr/models/pytorch-i3d/combined_resizer_56_wider_all_first'
     num_epochs=50
     run(data_root, model_input_shape, virat_model_path, batch_size, save_model, num_epochs=num_epochs)
