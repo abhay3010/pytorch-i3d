5c8e655bd4e8ae38a7dfefc6595a7faa6bbfd810
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-15 10:31:51 -0700

    added changes for the debug job

--- conf/debug_1.py
+++ conf/debug_1.py
@@ -2,7 +2,7 @@ opt = {
     "model":"spatial_resizer", 
     "params":{
         "model_prefix":"resizer_sp_1_", # the prefix used while training the model, 
-        "epoch_list": range(3,26), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "epoch": 19 , # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr/models/pytorch-i3d/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
@@ -13,6 +13,10 @@ opt = {
         "resize_shape":112, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
         "num_resblocks": 1, # the number of residual blocks being used in the model
+        "confusion":"confusion.npy", #the filename with which to save the confusion matrix for the test set 
+        "predictions":"predictions.npy", #the filename with which to save thepreductions for the test set 
+        "actuals":"actuals.npy", #the filename with which to save the actual labels for the test set 
+        "logits":"logits.npy" #the filename with which to save the logits for the test set 
     }
 
 }
