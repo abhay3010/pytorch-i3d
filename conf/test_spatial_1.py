51ba368fe90acfbfaec427bf94ec64e8d77d235d
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-15 18:37:55 -0700

    added changes for 2 residual blocks

--- conf/test_spatial_1.py
+++ conf/test_spatial_1.py
@@ -1,7 +1,7 @@
 opt = {
     "model":"spatial_resizer", 
     "params":{
-        "model_prefix":"resizer_spatial_1_", # the prefix used while training the model, 
+        "model_prefix":"resizer_spatial_2_", # the prefix used while training the model, 
         "epoch_list": range(3,26), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr/models/pytorch-i3d/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
@@ -12,7 +12,7 @@ opt = {
         "batch_size": 12, # batch size of the evaluation block, will depend on the gpu 
         "resize_shape":28, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
-        "num_resblocks": 1, # the number of residual blocks being used in the model
+        "num_resblocks": 2, # the number of residual blocks being used in the model
     }
 
 }
