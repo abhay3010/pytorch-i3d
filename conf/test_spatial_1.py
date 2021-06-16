6abd0a0f0e4bdfa5bc128ef9bb0a0132e8cccfe0
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-16 09:48:22 -0700

    added new config

--- conf/test_spatial_1.py
+++ conf/test_spatial_1.py
@@ -1,8 +1,8 @@
 opt = {
     "model":"spatial_resizer", 
     "params":{
-        "model_prefix":"resizer_spatial_3_", # the prefix used while training the model, 
-        "epoch_list": range(6,30), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "model_prefix":"resizer_spatial_1_56_", # the prefix used while training the model, 
+        "epoch_list": range(6,51), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr/models/pytorch-i3d/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
@@ -10,9 +10,9 @@ opt = {
         "num_frames": 32, #number of frames being given to the model at a time
         "num_workers": 4, # number of dataloader workers being used
         "batch_size": 12, # batch size of the evaluation block, will depend on the gpu 
-        "resize_shape":28, # initial shape being fed to the resizer model
+        "resize_shape":56, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
-        "num_resblocks": 3, # the number of residual blocks being used in the model
+        "num_resblocks": 1, # the number of residual blocks being used in the model
     }
 
 }
