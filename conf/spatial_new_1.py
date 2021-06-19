a55eab2eeac6b8cbdd6548cda0779b812828c42d
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-18 20:17:55 -0700

    updated eval config

--- conf/spatial_new_1.py
+++ conf/spatial_new_1.py
@@ -1,7 +1,7 @@
 opt = {
     "model":"spatial_resizer", 
     "params":{
-        "model_prefix":"resizer_spatial_corrected_1_56_first_last_", # the prefix used while training the model, 
+        "model_prefix":"resizer_spatial_corrected_1_56_first_first_", # the prefix used while training the model, 
         "epoch_list": range(2, 23), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr-models/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
@@ -13,7 +13,7 @@ opt = {
         "resize_shape":56, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
         "num_resblocks": 1, # the number of residual blocks being used in the model
-        "confusion":"twice_after_resize.npy"
+        "confusion":"corrected_frist_first.npy"
     }
 
 }
