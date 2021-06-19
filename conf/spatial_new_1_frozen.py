011bac3e9d93058c6df1c3e03a0f0de0c25b701b
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-19 14:12:19 -0700

    added  changes

--- conf/spatial_new_1_frozen.py
+++ conf/spatial_new_1_frozen.py
@@ -1,8 +1,8 @@
 opt = {
     "model":"spatial_resizer", 
     "params":{
-        "model_prefix":"resizer_spatial_corrected_1_56_after_first_first_frozen_", # the prefix used while training the model, 
-        "epoch_list": range(2, 30), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "model_prefix":"resizer_spatial_corrected_1_56_after_first_first_both_frozen_", # the prefix used while training the model, 
+        "epoch_list": range(2, 16), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr-models/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
