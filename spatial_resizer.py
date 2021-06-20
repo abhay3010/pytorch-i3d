553ae855a05eb8dfa95d0f07382fbdf036ec3598
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-20 07:58:50 -0700

    set align as false

--- spatial_resizer.py
+++ spatial_resizer.py
@@ -238,8 +238,8 @@ class SegmentedResizer(nn.Module):
         w = x.shape[4]
         #print(theta.shape)
         x_view = x.view(-1,c,h,w)
-        grid = F.affine_grid(theta, x_view.size(),align_corners=True)
-        x_view = F.grid_sample(x_view, grid, align_corners=True)
+        grid = F.affine_grid(theta, x_view.size(),align_corners=False)
+        x_view = F.grid_sample(x_view, grid, align_corners=False)
         o = x_view.view(b,c,t,h,w)
         return o
 
