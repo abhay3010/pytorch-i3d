55db1646149c713506fadd56f33dd1aeb2dc192b
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-21 13:34:11 -0700

    added after skip test conf

---
+++ conf/test_after_skip.py
@@ -0,0 +1,25 @@
+opt = {
+    "model":"spatial_resizer", 
+    "params":{
+        "model_prefix":"resizer_spatial_3dcnn_after_skip_", # the prefix used while training the model, 
+        "epoch_list": range(2,19), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "model_path":"/virat-vr-models/", # directory where the models are saved (remember the trailing /)
+        "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
+        "classes_file": "classes.txt", 
+        "i3d_mode" : "32x112", # i3d mode same as in the i3d test files
+        "num_frames": 32, #number of frames being given to the model at a time
+        "num_workers": 4, # number of dataloader workers being used
+        "batch_size": 60, # batch size of the evaluation block, will depend on the gpu 
+        "resize_shape":56, # initial shape being fed to the resizer model
+        "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
+        "num_resblocks": 1, # the number of residual blocks being used in the model
+        "confusion":"_corrected_after_3d_first_after_skip.npy",
+        "mode":"2d",
+        "apply_at":"after_skip"
+    }
+
+}
+
+def get():
+    global opt
+    return opt
