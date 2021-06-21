54c9a1a6a16f0c51c7f8d39340fc33c553dd65d3
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-21 09:15:44 -0700

    changed the channel depths

--- spatial_resizer.py
+++ spatial_resizer.py
@@ -48,13 +48,13 @@ class TransformerWithResizer(nn.Module):
         
         self.skip_resizer =  ResizerBlock((self.nframes,)+self.scale_shape, False)
         if not self.skip:
-            self.c1 = ConvUnit(in_channels=self.in_channels, output_channels=32, kernel_shape=[1, 7, 7], norm=None)
+            self.c1 = ConvUnit(in_channels=self.in_channels, output_channels=32, kernel_shape=[3, 7, 7], norm=None)
             #revisit size of this unit as it is inconsitent between paper and diagram
             self.c2 = ConvUnit(in_channels=32, kernel_shape = [1,1,1], output_channels=16)
             self.resizer_first = ResizerBlock((self.nframes,) + self.scale_shape, False)
-            self.residual_blocks = make_residuals_2d(num_resblocks, 16)
-            self.c3 = ConvUnit(in_channels=16, kernel_shape=[1,3,3], output_channels=16, lru=False)
-            self.c4 = ConvUnit(in_channels=16, kernel_shape=[1,7,7], output_channels=self.in_channels, lru=False, norm=None)
+            self.residual_blocks = make_residuals(num_resblocks, 16)
+            self.c3 = ConvUnit(in_channels=16, kernel_shape=[3,3,3], output_channels=16, lru=False)
+            self.c4 = ConvUnit(in_channels=16, kernel_shape=[3,7,7], output_channels=self.in_channels, lru=False, norm=None)
 
     def forward(self, x):
         residual = self.skip_resizer(x)        
@@ -63,17 +63,19 @@ class TransformerWithResizer(nn.Module):
         else:
             
             out = self.c1(x)
-            theta = self.get_theta(out)           
             out = self.c2(out)
+            theta = self.get_theta(out)           
+
             out =  self.resizer_first(out)
-            out = self.apply_theta(theta, out)
+            #out = self.apply_theta(theta, out)
             residual_skip = out
             out = self.residual_blocks(out)
             out = self.c3(out)
             out+=residual_skip
             out = self.c4(out)
-            out+=self.apply_theta(theta,residual)
-            
+            # out+=self.apply_theta(theta,residual)
+            out+=residual
+            out = self.apply_theta(theta, out)
             #print(theta.shape, out.shape)
             
             return out
