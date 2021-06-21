724d5ce22298e95b8136c110d17e3c3662223945
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-21 15:40:34 -0700

    Added changes

--- conf/test_after_residual.py
+++ conf/test_after_residual.py
@@ -1,8 +1,8 @@
 opt = {
     "model":"spatial_resizer", 
     "params":{
-        "model_prefix":"resizer_spatial_3dcnn_after_residual_skip_", # the prefix used while training the model, 
-        "epoch_list": range(0,17), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "model_prefix":"resizer_spatial_3dcnn_after_residual_skip_dual_", # the prefix used while training the model, 
+        "epoch_list": range(0,12), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr-models/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
@@ -13,7 +13,7 @@ opt = {
         "resize_shape":56, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
         "num_resblocks": 1, # the number of residual blocks being used in the model
-        "confusion":"_corrected_after_3d_first_after_residual_skip.npy",
+        "confusion":"_corrected_after_3d_first_after_residual_skip_dual.npy",
         "mode":"2d",
         "apply_at":"before_skip"
     }
