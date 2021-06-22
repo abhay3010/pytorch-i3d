6feee9d53daf04bdd63fa38a1599e19dcafb6f45
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-21 18:48:07 -0700

    added changes

--- conf/spatial_after_skip.py
+++ conf/spatial_after_skip.py
@@ -2,7 +2,7 @@ opt = {
     "model":'spatial_resizer',
     "params":{
     "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset, 
-    "save_path": "/virat-vr-models/resizer_spatial_3dcnn_after_skip_3res_", # prefix of the model being trained. Saved after every epoch, with the epoch id added 
+    "save_path": "/virat-vr-models/resizer_spatial_2dcnn_after_skip_3res_", # prefix of the model being trained. Saved after every epoch, with the epoch id added 
     # "save_path": "virat_vr/sample_model_",
     # "data_root": "TinyVIRAT/",
     "num_epochs": 50, #number of epochs to train for
