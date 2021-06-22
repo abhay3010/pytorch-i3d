d3e82a5d61945d0af038107e67cafcb130189293
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-22 13:16:33 -0700

    added changes

--- conf/test_3d_1.py
+++ conf/test_3d_1.py
@@ -1,10 +1,10 @@
 opt = {
     "model":"resizer", 
     "params":{
-        "model_prefix":"resizer_3d_1_unfrozen", # the prefix used while training the model, 
-        "model_type":"3d",
-        "epoch_list": range(4, 16), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
-        "model_path":"/virat-vr/models/pytorch-i3d/", # directory where the models are saved (remember the trailing /)
+        "model_prefix":"resizer_3d_3res_unfrozen_", # the prefix used while training the model, 
+        "model_type":"2d",
+        "epoch_list": range(3, 50), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "model_path":"/virat-vr-models/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
         "i3d_mode" : "32x112", # i3d mode same as in the i3d test files
@@ -13,7 +13,7 @@ opt = {
         "batch_size": 12, # batch size of the evaluation block, will depend on the gpu 
         "resize_shape":56, # initial shape being fed to the resizer model
         "model_input_shape":112, # final shape being output by the resizer and being fed to the i3d model
-        "num_resblocks": 1, # the number of residual blocks being used in the model
+        "num_resblocks": 3, # the number of residual blocks being used in the model
         "confusion":"3d_resizer_1.npy"
     }
 
