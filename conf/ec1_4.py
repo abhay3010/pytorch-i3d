41736b28ebbf9caaf511cac3630a05abc924cac0
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-23 20:12:38 -0700

    aded changes

--- conf/ec1_4.py
+++ conf/ec1_4.py
@@ -2,7 +2,7 @@ opt = {
     "model":"spatial_resizer", 
     "params":{
         "model_prefix":"resizer_spatial_1_4_3res_", # the prefix used while training the model, 
-        "epoch_list": range(0,23), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
+        "epoch_list": range(0,30), # the epochs for which the model needs to be evaluated, currently epoch 3 and 10
         "model_path":"/virat-vr-r-models/", # directory where the models are saved (remember the trailing /)
         "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset
         "classes_file": "classes.txt", 
