0a29fe9ae442a8141f71a6d9d74a33ea6fdbb4e9
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-06-24 19:11:12 -0700

    added 28 config

--- conf/c2_8.py
+++ conf/c2_8.py
@@ -2,7 +2,7 @@ opt = {
     "model":'spatial_resizer',
     "params":{
     "data_root":"/mnt/data/TinyVIRAT/", # root folder to the tinyVIRAT dataset, 
-    "save_path": "/virat-vr-r-models/resizer_spatial_2_7_3res_", # prefix of the model being trained. Saved after every epoch, with the epoch id added 
+    "save_path": "/virat-vr-r-models/resizer_spatial_2_8_3res_", # prefix of the model being trained. Saved after every epoch, with the epoch id added 
     # "save_path": "virat_vr/sample_model_",
     # "data_root": "TinyVIRAT/",
     "num_epochs": 30, #number of epochs to train for
@@ -20,7 +20,7 @@ opt = {
     "num_steps_per_update":8,
     "mode":'2d',
     "read_at":2, 
-    "apply_at":7
+    "apply_at":8
     }
 }
 def get():
